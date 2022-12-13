13e6f2ccf9a9d756f4b747a314cf39cdf2e2f29b
Alejandro Wilcke <aleee9208@hotmail.com>
2022-12-13 09:44:59 -0300

    fix: image domain next config

--- next.config.js
+++ next.config.js
@@ -3,13 +3,7 @@ const nextConfig = {
   reactStrictMode: false, // If set true, double rendering in development mode (NextJS)
   swcMinify: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com', // URL for getting alternative static pokemon images.
-        port: '',
-      },
-    ],
+    domains: ["raw.githubusercontent.com"]
   },
 }
 
